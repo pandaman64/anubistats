8c194c4125eced551619e0858c3c9e06353250fc
pandaman64 <kointosudesuyo@infoseek.jp>
2023-05-01 19:59:21 +0900

    Write and read columnar data

--- crates/anubistats/src/main.rs
+++ crates/anubistats/src/main.rs
@@ -2,64 +2,107 @@
 
 mod repl;
 
-use std::{collections::BTreeMap, fs::File, io::BufWriter};
+use std::{collections::BTreeMap, fs::File, io::BufWriter, sync::Arc};
 
 use anubistats::read_datasets;
+use arrow::{
+    array::{StringBuilder, UInt32Builder, UInt64Builder},
+    datatypes::{DataType, Field, Schema},
+    record_batch::RecordBatch,
+};
+use parquet::arrow::ArrowWriter;
 use roaring::RoaringBitmap;
 
 fn main() -> anyhow::Result<()> {
     // Construct postings lists from the words in the titles.
     let mut postings_lists = BTreeMap::new();
-    let mut roaring_id = 0;
-    for record in read_datasets()? {
-        roaring_id += 1;
+    let mut id_builder = UInt32Builder::new();
+    let mut doc_id_builder = UInt64Builder::new();
+    let mut title_builder = StringBuilder::new();
+
+    for (roaring_id, record) in (read_datasets()?).enumerate() {
         let record = record?;
+
+        // Add to postings lists
         for word in record.title.split_whitespace() {
             let word = word.to_lowercase();
             if !word.is_empty() {
                 let postings_list = postings_lists
                     .entry(word)
                     .or_insert_with(RoaringBitmap::new);
-                postings_list.push(roaring_id);
+                postings_list.push(roaring_id.try_into()?);
             }
         }
+
+        // Add to columnar store
+        id_builder.append_value(roaring_id.try_into()?);
+        doc_id_builder.append_value(record.id);
+        title_builder.append_value(record.title);
     }
 
     let postings_lists_file = File::create("postings_lists.bin")?;
     let mut postings_lists_writer = BufWriter::new(postings_lists_file);
     let mut postings_lists_offsets = BTreeMap::new();
 
+    let mut word_builder = StringBuilder::new();
+    let mut offset_builder = UInt64Builder::new();
+    let mut length_builder = UInt64Builder::new();
+
     let mut offset = 0;
     for (word, postings_list) in postings_lists {
         postings_list.serialize_into(&mut postings_lists_writer)?;
-        postings_lists_offsets.insert(word, offset);
+        postings_lists_offsets.insert(word.clone(), offset);
         offset += postings_list.serialized_size();
+
+        word_builder.append_value(word);
+        offset_builder.append_value(offset.try_into()?);
+        length_builder.append_value(postings_list.serialized_size().try_into()?);
     }
 
     let postings_lists_offsets_file = File::create("postings_lists_offsets.json")?;
-    serde_json::to_writer_pretty(postings_lists_offsets_file, &postings_lists_offsets)?;
+    serde_json::to_writer(postings_lists_offsets_file, &postings_lists_offsets)?;
 
-    // REPL for querying the postings lists.
-    // let mut input = String::new();
-    // loop {
-    //     println!("Enter a word to query:");
-    //     input.clear();
-    //     std::io::stdin().read_line(&mut input)?;
-    //     let input = input.trim();
-    //     if input.is_empty() {
-    //         break;
-    //     }
-    //     let postings_list = postings_lists.get(input);
-    //     if let Some(postings_list) = postings_list {
-    //         println!("{} documents contain the word '{}'", postings_list.len(), input);
-    //         // println!("The documents are:");
-    //         // for id in postings_list {
-    //         //     println!("  {}", id);
-    //         // }
-    //     } else {
-    //         println!("No documents contain the word '{}'", input);
-    //     }
-    // }
+    let schema = Schema::new(vec![
+        Field::new("id", DataType::UInt32, false),
+        Field::new("doc_id", DataType::UInt64, false),
+        Field::new("title", DataType::Utf8, false),
+    ]);
+    let batch = RecordBatch::try_new(
+        Arc::new(schema),
+        vec![
+            Arc::new(id_builder.finish()),
+            Arc::new(doc_id_builder.finish()),
+            Arc::new(title_builder.finish()),
+        ],
+    )?;
+
+    let ids_file = File::create("stored_fields.parquet")?;
+    let mut writer = ArrowWriter::try_new(ids_file, batch.schema(), None)?;
+    writer.write(&batch)?;
+    writer.close()?;
+
+    let word_offset_schema = Schema::new(vec![
+        Field::new("word", DataType::Utf8, false),
+        Field::new("offset", DataType::UInt64, false),
+        Field::new("length", DataType::UInt64, false),
+    ]);
+    let word_batch = RecordBatch::try_new(
+        Arc::new(word_offset_schema),
+        vec![
+            Arc::new(word_builder.finish()),
+            Arc::new(offset_builder.finish()),
+            Arc::new(length_builder.finish()),
+        ],
+    )?;
+
+    let postings_list_offsets_parquet_file = File::create("postings_lists_offsets.parquet")?;
+    let mut writer = ArrowWriter::try_new(
+        postings_list_offsets_parquet_file,
+        word_batch.schema(),
+        None,
+    )?;
+    writer.write(&word_batch)?;
+    writer.close()?;
 
     Ok(())
 }
--- crates/anubistats/src/repl.rs
+++ crates/anubistats/src/repl.rs
@@ -6,6 +6,11 @@ use std::{
 };
 
 use anubistats_query::Query;
+use arrow::array::{AsArray, BooleanArray, StringArray, UInt32Array, UInt64Array};
+use parquet::arrow::{
+    arrow_reader::{ArrowPredicateFn, ParquetRecordBatchReaderBuilder, RowFilter},
+    ProjectionMask,
+};
 use roaring::RoaringBitmap;
 
 fn find_offset_and_length(
@@ -62,6 +67,63 @@ fn eval_query(
     }
 }
 
+struct Document {
+    roaring_id: u32,
+    doc_id: u64,
+    title: String,
+}
+
+fn retrieve_stored_fields(roaring_ids_filter: RoaringBitmap) -> anyhow::Result<Vec<Document>> {
+    let len = roaring_ids_filter.len();
+
+    let file = File::open("stored_fields.parquet")?;
+    let builder = ParquetRecordBatchReaderBuilder::try_new(file)?;
+
+    // Construct a reader that only reads the rows that have matching roaring IDs.
+    let predicate = ArrowPredicateFn::new(
+        ProjectionMask::leaves(
+            builder.parquet_schema(),
+            std::iter::once(
+                builder
+                    .parquet_schema()
+                    .columns()
+                    .iter()
+                    .position(|c| c.name() == "id")
+                    .unwrap(),
+            ),
+        ),
+        move |batch| {
+            let roaring_ids: &UInt32Array = batch.column(0).as_primitive();
+            Ok(BooleanArray::from_unary(roaring_ids, |roaring_id| {
+                roaring_ids_filter.contains(roaring_id)
+            }))
+        },
+    );
+    let row_filter = RowFilter::new(vec![Box::new(predicate)]);
+    let reader = builder.with_row_filter(row_filter).build()?;
+
+    let mut documents = Vec::with_capacity(len.try_into()?);
+    for batch in reader {
+        let batch = batch?;
+        let roaring_ids: &UInt32Array = batch["id"].as_primitive();
+        let doc_ids: &UInt64Array = batch["doc_id"].as_primitive();
+        let title: &StringArray = batch["title"].as_string();
+
+        for i in 0..batch.num_rows() {
+            let roaring_id = roaring_ids.value(i);
+            let doc_id = doc_ids.value(i);
+            let title = title.value(i);
+
+            documents.push(Document {
+                roaring_id,
+                doc_id,
+                title: title.to_string(),
+            });
+        }
+    }
+    Ok(documents)
+}
+
 fn main() -> anyhow::Result<()> {
     // Read postings lists and index from disk.
     let postings_lists_file = File::open("postings_lists.bin")?;
@@ -88,6 +150,14 @@ fn main() -> anyhow::Result<()> {
             postings_lists.len(),
             query
         );
+
+        let documents = retrieve_stored_fields(postings_lists)?;
+        for document in documents.iter().take(10) {
+            println!(
+                "[{}] {}: {}",
+                document.roaring_id, document.doc_id, document.title
+            );
+        }
     }
 
     Ok(())
